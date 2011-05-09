cac7d03329574ea76acfd5a9844174335e827c6f
Chukwuma Akpuokwe <cakpuokwe@peak6.com>
2011-05-09 13:19:51 -0500

    version 0.2.5

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import os
 from setuptools import setup
 
 setup(name = "pymmd",
-      version = "0.2.4",
+      version = "0.2.5",
       description = "An MMD library for Python",
       packages = ['pymmd'])
