dd5d1f8f6a42ad9f7a1195071d0606e49117b2b4
Scott R. Parish <sparish@peak6.com>
2011-09-01 10:48:07 -0500

    version 0.2.7

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import os
 from setuptools import setup
 
 setup(name = "pymmd",
-      version = "0.2.5",
+      version = "0.2.7",
       description = "An MMD library for Python",
       packages = ['pymmd'])
